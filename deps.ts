717e0918823f0d25c6fa03117b1b96da58e5f661
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-11-28 09:55:28 +0900

    done :完成

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
 // export { parse } from "https://deno.land/std@0.115.1/flags/mod.ts";
+export { Command } from "https://deno.land/x/cliffy@v0.20.1/command/mod.ts";
--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log("This is main script!");
+import { Command } from "./deps.ts";
+import { GetGhStarCount } from "./src/getRepo.ts";
+async function main() {
+  const { args } = await new Command()
+    .arguments("<repoName>")
+    .description("get repo url adn star count")
+    .parse(Deno.args);
+  console.log("star count = " + (await GetGhStarCount(args[0])));
+}
+
+main();
